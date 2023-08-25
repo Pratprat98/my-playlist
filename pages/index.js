278a4d97543e94bfc8367fa0b11d9de3c3fe3351
Pratprat98 <86490624+Pratprat98@users.noreply.github.com>
2023-08-25 10:55:51 -0500

    8/25/2023 10:55AM

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,6 @@ const Index = () => {
   const [songInfo, setSongInfo] = useState([]);
   const [title, setTitle] = useState("");
 
-  const handleAddSong = (e) => {
-    e.preventDefault();
-    const songName = e.target.songName.value;
-    if (songName) {
-      setPlaylistSongs([...playlistSongs, songName]);
-      e.target.songName.value = "";
-    }
-  };
-
   const savePlaylist = () => {
     if (playlistName && playlistSongs.length > 0) {
       setSavedPlaylists([
@@ -59,63 +50,28 @@ const Index = () => {
     }
   };
 
-  const addToPlaylist = (e) => {
-    const songName = e.target.innerText;
-    if (songName) {
-      setPlaylistSongs([...playlistSongs, songName]);
-    }
+  const addToPlaylist = (songName, artistName) => {
+    return (e) => {
+      e.preventDefault();
+      const song = `${songName} - ${artistName}`;
+      setPlaylistSongs([...playlistSongs, song]);
+    };
   };
+
+  useEffect(() => {
+    const savedPlaylists = JSON.parse(localStorage.getItem("savedPlaylists"));
+    if (savedPlaylists) {
+      setSavedPlaylists(savedPlaylists);
+    }
+  }, []);
 
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1>Playlist Creator</h1>
+        <h1>Create Your Playlist</h1>
       </header>
-      <div className="app-content">
-        <div className="playlist-section">
-          <h2>Create Playlist</h2>
-          <button className="playlist-name-button" onClick={() => setPlaylistName(prompt("Enter playlist name"))}>
-            {playlistName || "Enter Playlist Name"}
-          </button>
-          <ul className="playlist-songs-list">
-            {playlistSongs.map((song, index) => (
-              <li key={index} className="playlist-song">{song}</li>
-            ))}
-          </ul>
-          <button className="save-button" onClick={savePlaylist}>Save Playlist</button>
-        </div>
-        <div className="search-section">
-          <h2>Search for a Song</h2>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              getSongInfo();
-            }}
-          >
-            <input
-              type="text"
-              name="title"
-              placeholder="Enter song title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <button className="search-button" type="submit">Search</button>
-          </form>
-          <h2>Song Information</h2>
-          <div className="song-grid">
-            {songInfo.map((song, index) => (
-              <div className="song-card" key={index}>
-                <img src={song.album.cover_medium} alt="album cover" />
-                <p>
-                  <button className="add-to-playlist-button" onClick={addToPlaylist}>{song.title}</button>
-                </p>
-                <p>Duration: {song.duration} seconds</p>
-                <p>Album: {song.album.title}</p>
-                <p>Artist: {song.artist.name}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+
+      <div className="playlist-control">
         <div className="saved-playlists-section">
           <h2>Saved Playlists</h2>
           <ul className="saved-playlists-list">
@@ -123,24 +79,99 @@ const Index = () => {
               <li key={index} className="saved-playlist">
                 <span
                   onClick={() => {
-                    savedPlaylists[index].expanded = !savedPlaylists[index].expanded;
+                    savedPlaylists[index].expanded =
+                      !savedPlaylists[index].expanded;
                     setSavedPlaylists([...savedPlaylists]);
                   }}
                   className="playlist-name"
                 >
                   {playlist.name}
                 </span>
-                <button className="delete-button" onClick={() => deletePlaylist(index)}>Delete Playlist</button>
+                <button
+                  className="delete-button"
+                  onClick={() => deletePlaylist(index)}
+                >
+                  Delete Playlist
+                </button>
                 {playlist.expanded && (
                   <ul className="playlist-songs-list">
                     {playlist.songs.map((song, songIndex) => (
-                      <li key={songIndex} className="playlist-song">{song}</li>
+                      <li key={songIndex} className="playlist-song">
+                        {song}
+                      </li>
                     ))}
                   </ul>
                 )}
               </li>
             ))}
           </ul>
+        </div>
+        <div className="gap">
+        </div>
+        <button
+            onClick={() => setPlaylistName(prompt("Enter playlist name"))}
+          >
+            {playlistName || "Enter Playlist Name"}
+          </button>
+
+        <ul className="playlist-songs-list">
+          {playlistSongs.map((song, index) => (
+            <li key={index} className="playlist-song">
+              {song}
+            </li>
+          ))}
+        </ul>
+        <button onClick={savePlaylist}>
+          Save Playlist
+        </button>
+      </div>
+      <div className="gap">
+        </div>
+      <div className="search-section">
+        <div className="search-bar">
+          <h2>Search for a Song</h2>
+          <div className="gap">
+        </div>
+          <div className="search-container">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                getSongInfo();
+              }}
+            >
+              <input
+                type="text"
+                name="title"
+                // Make this appear centered placeholder="Enter song title"
+                placeholder="Enter song title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              
+              <button type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+        <div className="gap">
+        </div>
+        <h2>Song Information</h2>
+        <div className="song-grid">
+          {songInfo.map((song, index) => (
+            <div className="song-card" key={index}>
+              <img src={song.album.cover_medium} alt="album cover" />
+              <p>
+                <button
+                  className="add-to-playlist-button"
+                  onClick={addToPlaylist(song.title, song.artist.name)}
+                >
+                  {song.title}
+                </button>
+              </p>
+              <p>Duration: {song.duration} seconds</p>
+              <p>Album: {song.album.title}</p>
+              <p>Artist: {song.artist.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
