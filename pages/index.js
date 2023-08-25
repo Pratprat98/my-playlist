849974259b8166efa62fd4215b96568d98465f38
Pratprat98 <86490624+Pratprat98@users.noreply.github.com>
2023-08-24 20:22:02 -0500

    commit 1

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,80 @@
 import React, { useState } from "react";
 
 const Index = () => {
-  const [notes, setNotes] = useState([]);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [playlistName, setPlaylistName] = useState("");
+  const [playlistSongs, setPlaylistSongs] = useState([]);
+  const [savedPlaylists, setSavedPlaylists] = useState([]);
 
-  const handleTitleChange = (e) => {
-    if (e.target.name === "title") setTitle(e.target.value);
-  };
-  const handleContentChange = (e) => {
-    if (e.target.name === "content") setContent(e.target.value);
-  };
-
-  const addNotes = () => {
-    setNotes([...notes, { title, content }]);
-    setTitle("");
-    setContent("");
+  const handleAddSong = (e) => {
+    e.preventDefault();
+    const songName = e.target.songName.value;
+    if (songName) {
+      setPlaylistSongs([...playlistSongs, songName]);
+      e.target.songName.value = "";
+    }
   };
 
-  const deleteNotes = (index) => {
-    const updatedNotes = notes.filter((_, i) => i !== index);
-    setNotes(updatedNotes);
+  const savePlaylist = () => {
+    if (playlistName && playlistSongs.length > 0) {
+      setSavedPlaylists([...savedPlaylists, { name: playlistName, songs: playlistSongs }]);
+      setPlaylistName("");
+      setPlaylistSongs([]);
+    }
+  };
+
+  const deletePlaylist = (index) => {
+    const updatedPlaylists = savedPlaylists.filter((_, i) => i !== index);
+    setSavedPlaylists(updatedPlaylists);
   };
 
   return (
     <>
       <header>
         <h1>Playlist Creator</h1>
-        <input type="text" id="searchInput" placeholder="Search for a song" />
+        <button onClick={() => setPlaylistName(prompt("Enter playlist name"))}>
+          Create Playlist
+        </button>
       </header>
       <main>
-        <div id="searchResults" class="result-div"></div>
-        <div id="playlist" class="result-div">
+        <div className="result-div">
           <h2>Your Playlist</h2>
-          <ul id="playlistItems"></ul>
+          <h3>{playlistName}</h3>
+          <form onSubmit={handleAddSong}>
+            <input type="text" name="songName" placeholder="Enter song name" />
+            <button type="submit">Add Song</button>
+          </form>
+          <ul>
+            {playlistSongs.map((song, index) => (
+              <li key={index}>{song}</li>
+            ))}
+          </ul>
+          <button onClick={savePlaylist}>Save Playlist</button>
+        </div>
+        <div className="result-div">
+          <h2>Saved Playlists</h2>
+          <ul>
+            {savedPlaylists.map((playlist, index) => (
+              <li key={index}>
+                <span
+                  onClick={() => {
+                    savedPlaylists[index].expanded = !savedPlaylists[index].expanded;
+                    setSavedPlaylists([...savedPlaylists]);
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  {playlist.name}
+                </span>
+                <button onClick={() => deletePlaylist(index)}>Delete Playlist</button>
+                {playlist.expanded && (
+                  <ul>
+                    {playlist.songs.map((song, songIndex) => (
+                      <li key={songIndex}>{song}</li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
